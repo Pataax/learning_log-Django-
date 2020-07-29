25149d3d316ae58178081e736cfd05d4f0a9da08
bruno <email@email.com>
2020-07-29 17:40:42 -0300

    Construindo html

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
 
+app_name = 'learning_logs'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'', include('learning_logs.urls', namespace='learning_logs')),
 ]
